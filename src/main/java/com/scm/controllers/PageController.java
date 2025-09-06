bf6f21b93a4998195485025a67788b0933c4a5cc
komalsingh02 <komalsingh23748@gmail.com>
2025-09-06 16:32:14 +0530

    feat(home): render dynamic data using Model and thymeleaf

--- src/main/java/com/scm/controllers/PageController.java
+++ src/main/java/com/scm/controllers/PageController.java
@@ -1,16 +1,20 @@
 package com.scm.controllers;
-
-
-import org.springframework.stereotype.Controller; 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class PageController {
 
     @RequestMapping("/home")
-    public String home()
+    public String home(Model model)
     {
+
         System.out.println("home page handler ");
+        model.addAttribute("name","Komal");
+        model.addAttribute("email","komal@gmail.com");
+        model.addAttribute("address","XYZZZ");
+        model.addAttribute("githublink","https://github.com/komalsingh02/smart-contact-manager");
         return "home";
     }
 }
